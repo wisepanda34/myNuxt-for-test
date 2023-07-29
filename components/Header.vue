0398e9d7a45653bea945abe5c9051a99a9ececf8
tylkeen1803 <evghen.naydenko@gmail.com>
2023-07-29 20:25:21 +0300

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="header">
+    <div class="logo">LOGO</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
