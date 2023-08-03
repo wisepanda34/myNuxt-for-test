8bf42dcb8481a3838adb40371a208407d5c8d089
tylkeen1803 <evghen.naydenko@gmail.com>
2023-08-03 22:47:48 +0300

    added vuelidate in login.vue

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,14 @@
                     <path fill="currentColor" d="M9.775 12q-.9 0-1.5-.675T7.8 9.75l.325-2.45q.2-1.425 1.3-2.363T12 4q1.475 0 2.575.938t1.3 2.362l.325 2.45q.125.9-.475 1.575t-1.5.675h-4.45Zm0-2h4.45L13.9 7.6q-.1-.7-.637-1.15T12 6q-.725 0-1.263.45T10.1 7.6L9.775 10ZM12 10ZM4 20v-2.8q0-.85.438-1.563T5.6 14.55q1.55-.775 3.15-1.163T12 13q1.65 0 3.25.388t3.15 1.162q.725.375 1.163 1.088T20 17.2V20H4Zm2-2h12v-.8q0-.275-.138-.5t-.362-.35q-1.35-.675-2.725-1.012T12 15q-1.4 0-2.775.338T6.5 16.35q-.225.125-.363.35T6 17.2v.8Zm6 0Z"/>
                   </svg>
                 </div>
-                <input type="text" id="name" name="name" required class="border-gray-300 pl-7 rounded-md focus:border-green-500 focus:ring-green-500 text-sm w-full" placeholder="John Doe">
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    required
+                    class="border-gray-300 pl-7 rounded-md focus:border-green-500 focus:ring-green-500 text-sm w-full"
+                    placeholder="John Doe"
+                >
               </div>
             </div>
 
@@ -35,7 +42,14 @@
                     <path fill="currentColor" d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 14H4V8l8 5l8-5v10zm-8-7L4 6h16l-8 5z"/>
                   </svg>
                 </div>
-                <input type="email" id="email" name="email" class="border-gray-300 px-7 rounded-md focus:border-gray-500 focus:ring-gray-500 text-sm text-gray-700 placeholder-gray-500 w-full" placeholder="JohnDoe@gmail.com">
+                <input
+                    type="email"
+                    id="email"
+                    required
+                    name="email"
+                    class="border-gray-300 px-7 rounded-md focus:border-gray-500 focus:ring-gray-500 text-sm text-gray-700 placeholder-gray-500 w-full"
+                    placeholder="JohnDoe@gmail.com"
+                >
               </div>
             </div>
 
@@ -48,7 +62,14 @@
                     <path fill="currentColor" d="M6 22q-.825 0-1.413-.588T4 20V10q0-.825.588-1.413T6 8h1V6q0-2.075 1.463-3.538T12 1q2.075 0 3.538 1.463T17 6v2h1q.825 0 1.413.588T20 10v10q0 .825-.588 1.413T18 22H6Zm0-2h12V10H6v10Zm6-3q.825 0 1.413-.588T14 15q0-.825-.588-1.413T12 13q-.825 0-1.413.588T10 15q0 .825.588 1.413T12 17ZM9 8h6V6q0-1.25-.875-2.125T12 3q-1.25 0-2.125.875T9 6v2ZM6 20V10v10Z"/>
                   </svg>
                 </div>
-                <input type="password" id="password" name="password" minLength="8" class="border-gray-300 pl-7 rounded-md focus:border-green-500 focus:ring-green-500 text-sm w-full" placeholder="min 4 characters">
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    minLength="8"
+                    class="border-gray-300 pl-7 rounded-md focus:border-green-500 focus:ring-green-500 text-sm w-full"
+                    placeholder="min 4 characters"
+                >
               </div>
             </div>
 
@@ -61,18 +82,33 @@
                     <path fill="currentColor" d="M6 22q-.825 0-1.413-.588T4 20V10q0-.825.588-1.413T6 8h1V6q0-2.075 1.463-3.538T12 1q2.075 0 3.538 1.463T17 6v2h1q.825 0 1.413.588T20 10v10q0 .825-.588 1.413T18 22H6Zm0-2h12V10H6v10Zm6-3q.825 0 1.413-.588T14 15q0-.825-.588-1.413T12 13q-.825 0-1.413.588T10 15q0 .825.588 1.413T12 17ZM9 8h6V6q0-1.25-.875-2.125T12 3q-1.25 0-2.125.875T9 6v2ZM6 20V10v10Z"/>
                   </svg>
                 </div>
-                <input type="password" id="password_confirmation" name="password_confirmation" minLength="8" class="border-gray-300 pl-7 rounded-md focus:border-green-500 focus:ring-green-500 text-sm w-full" placeholder="confirm password">
+                <input
+                    type="password"
+                    id="password_confirmation"
+                    name="password_confirmation"
+                    minLength="8"
+                    class="border-gray-300 pl-7 rounded-md focus:border-green-500 focus:ring-green-500 text-sm w-full"
+                    placeholder="confirm password"
+                >
               </div>
             </div>
 
-            <div class="pt-5">
-              <nuxt-link to="/verify-email" class="flex justify-center items-center rounded-md bg-green-600 py-2 px-4 text-white hover:bg-green-700 font-semibold
+<!--            <div class="pt-5">-->
+<!--              <nuxt-link to="/verify-email" class="flex justify-center items-center rounded-md bg-green-600 py-2 px-4 text-white hover:bg-green-700 font-semibold-->
+<!--          shadow-lg hover:shadow-xl focus:shadow-xl focus:outline-none focus:ring-2 focus:ring-green-500-->
+<!--          focus:ring-offset-2 transition duration-200 ease-in-out "-->
+<!--              >-->
+<!--                Sign up-->
+<!--              </nuxt-link>-->
+<!--            </div>-->
+            <button
+                type="submit"
+                class="flex justify-center items-center  mx-auto rounded-md bg-green-600 py-2 px-4 text-white hover:bg-green-700 font-semibold
           shadow-lg hover:shadow-xl focus:shadow-xl focus:outline-none focus:ring-2 focus:ring-green-500
           focus:ring-offset-2 transition duration-200 ease-in-out "
-              >
-                Sign up
-              </nuxt-link>
-            </div>
+            >
+              Submit
+            </button>
 
           </form>
 
--- pages/login.vue
+++ pages/login.vue
@@ -14,7 +14,13 @@
       </div>
       <div class="bg-white/50 backdrop-blur-xl mt-10 mx-auto rounded-xl shadow-xl p-5 sm:p-10 w-150">
 
-        <form action="#" autocomplete="off" class="space-y-6">
+        <form
+            action="#"
+            autocomplete="off"
+            class="space-y-6"
+            novalidate
+            @submit.prevent="submitLogin"
+        >
 
           <!--          warning-->
           <div class="flex gap-4 items-center justify-between border border-red-200 rounded p-3 bg-red-50">
@@ -27,14 +33,26 @@
 
           <!--            input Email-->
           <div>
-            <label class="block text-sm font-medium text-gray-400 pl-1" for="email">Email</label>
+            <label class="block text-sm font-medium text-gray-700 pl-1" for="email">Email</label>
             <div class="relative rounded-md shadow-md mt-2">
               <div class="absolute left-0 inset-y-0 flex items-center  pl-1.5">
                 <svg class="text-gray-400 w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <path fill="currentColor" d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 14H4V8l8 5l8-5v10zm-8-7L4 6h16l-8 5z"/>
                 </svg>
               </div>
-              <input type="email" id="email" name="email" class="border-gray-300 pl-7 rounded-md focus:border-green-500 focus:ring-green-500 text-sm w-full" placeholder="JohnDoe@gmail.com">
+              <input
+                  v-model="formData.email"
+                  class="border-gray-300 pl-7 rounded-md focus:border-green-500 focus:ring-green-500 text-sm w-full"
+                  :class="{
+                    'border-red-500 focus:border-red-500': v$.email.$error,
+                    'border-[#42d392] ': !v$.email.$invalid,
+                  }"
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="JohnDoe@gmail.com"
+                  @change="v$.email.$touch"
+              />
             </div>
           </div>
 
@@ -47,28 +65,55 @@
                   <path fill="currentColor" d="M6 22q-.825 0-1.413-.588T4 20V10q0-.825.588-1.413T6 8h1V6q0-2.075 1.463-3.538T12 1q2.075 0 3.538 1.463T17 6v2h1q.825 0 1.413.588T20 10v10q0 .825-.588 1.413T18 22H6Zm0-2h12V10H6v10Zm6-3q.825 0 1.413-.588T14 15q0-.825-.588-1.413T12 13q-.825 0-1.413.588T10 15q0 .825.588 1.413T12 17ZM9 8h6V6q0-1.25-.875-2.125T12 3q-1.25 0-2.125.875T9 6v2ZM6 20V10v10Z"/>
                 </svg>
               </div>
-              <input type="password" id="password" name="password" minLength="8" class="border-gray-300 pl-7 rounded-md focus:border-green-500 focus:ring-green-500 text-sm w-full" placeholder="min 4 characters">
+              <input
+                  v-model="formData.password"
+                  class="border-gray-300 pl-7 rounded-md focus:border-green-500 focus:ring-green-500 text-sm w-full"
+                  :class="{
+                    'border-red-500 focus:border-red-500': v$.password.$error,
+                    'border-[#42d392] ': !v$.password.$invalid,
+                  }"
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="min 4 characters"
+                  @change="v$.password.$touch"
+              >
             </div>
+
           </div>
 
           <div class="flex flex-col gap-3 justify-center items-center mt-5">
             <div class="flex gap-2 items-center justify-start">
 
-              <input class="w-4 h-4 rounded border-gray-300 text-green-600 focus:ring-0" type="checkbox" name="remember" id="remember"/>
+              <input
+                  class="w-4 h-4 rounded border-gray-300 text-green-600 focus:ring-0"
+                  type="checkbox"
+                  name="remember"
+                  id="remember"
+              />
               <label class="text-gray-900 text-sm" for="remember">Remember me</label>
             </div>
 
             <nuxt-link to="/forgot-password" class="font-medium text-sm text-green-600 hover:text-green-700">Forgot your password?</nuxt-link>
           </div>
 
-          <div class="pt-5">
-            <nuxt-link to="/forgot-password" class="flex justify-center items-center rounded-md bg-green-600 py-2 px-4 text-white hover:bg-green-700 font-semibold
-          shadow-lg hover:shadow-xl focus:shadow-xl focus:outline-none focus:ring-2 focus:ring-green-500
-          focus:ring-offset-2 transition duration-200 ease-in-out "
-            >
-              Sign in
-            </nuxt-link>
-          </div>
+<!--          <div class="pt-5">-->
+<!--            <nuxt-link to="/forgot-password" class="flex justify-center items-center rounded-md bg-green-600 py-2 px-4 text-white hover:bg-green-700 font-semibold-->
+<!--          shadow-lg hover:shadow-xl focus:shadow-xl focus:outline-none focus:ring-2 focus:ring-green-500-->
+<!--          focus:ring-offset-2 transition duration-200 ease-in-out "-->
+<!--            >-->
+<!--              Sign in-->
+<!--            </nuxt-link>-->
+<!--          </div>-->
+          <button
+              type="submit"
+              class="flex justify-center items-center mx-auto rounded-md bg-green-600 py-2 px-4 text-white hover:bg-green-700 font-semibold
+              shadow-lg hover:shadow-xl focus:shadow-xl focus:outline-none focus:ring-2 focus:ring-green-500
+              focus:ring-offset-2 transition duration-200 ease-in-out"
+          >
+            <!--              @click="$router.push('/forgot-password')"-->
+            Submit
+          </button>
 
         </form>
 
@@ -78,15 +123,65 @@
 </template>
 
 
-<script>
-export default {
-  name: "login",
-  data () {
-    return {
-      signUp: true
+<script setup>
+import { required, email, sameAs, minLength, helpers } from '@vuelidate/validators'
+import { useVuelidate } from '@vuelidate/core'
+
+
+
+const formData = reactive({
+  email: '',
+  password: ''
+})
+const signUp = true
+
+// const rules = computed(() => {
+//   return {
+//     email: { required, email },
+//     password: { required, minLength: minLength(6) }
+//     // confirmPassword: { required, sameAs: sameAs(formData.password) }
+//   }
+// })
+
+const rules = computed(() => {
+  return {
+    email: {
+      required: helpers.withMessage('The email field is required}}}}', required),
+      email: helpers.withMessage('Invalid email format/////', email),
+    },
+    password: {
+      required: helpers.withMessage('The password field is required!!!!!', required),
+      minLength: minLength(6),
     }
+    // confirmPassword: {
+    //   required: helpers.withMessage('The password confirmation field is required', required),
+    //   sameAs: helpers.withMessage("Passwords don't match", sameAs(formData.password)),
+    // },
   }
+})
+const v$ = useVuelidate(rules, formData)
+
+const submitLogin = () => {
+  if (v$.value.$invalid) return
+  console.log('data was sended')
+  // v$.value.$dirty = false
+  formData.email = ''
+  formData.password = ''
 }
+
+
+
+// export default {
+//   name: "login",
+//   data () {
+//     return {
+//       signUp: true
+//     }
+//   }
+// }
+
+
+
 </script>
 
 <style scoped>
