e076810c9be0bbc0b6f2ae71575db1ab24af9efe
tylkeen1803 <evghen.naydenko@gmail.com>
2023-08-04 20:02:45 +0300

    added page register - works,  index.vue - main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,121 +1,16 @@
 <template>
   <div>
     <Header :signIn="signIn"/>
-
-    <main class="flex flex-col justify-center pb-20">
-      <div class="mx-auto max-w-lg">
-        <svg class="text-green-600 mx-auto" xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24">
-          <path fill="currentColor" d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 3c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm7 13H5v-.23c0-.62.28-1.2.76-1.58C7.47 15.82 9.64 15 12 15s4.53.82 6.24 2.19c.48.38.76.97.76 1.58V19z"/>
-        </svg>
-        <h2 class="text-2xl sm:text-3xl font-bold text-gray-800">Create your profile</h2>
-      </div>
-      <div class="bg-white/50 backdrop-blur-xl mt-10 mx-auto rounded-xl shadow-xl p-5 sm:p-10 w-150">
-
-        <form action="#"
-              autocomplete="off"
-              class="space-y-6"
-              novalidate
-              @submit.prevent="submitRegistration"
-        >
-          <div v-for="(field, i) in Object.values(formData)" :key="i">
-            <label class="block text-sm font-medium text-gray-700 pl-1" :for="field.name">{{ field.name }}</label>
-            <div class="relative rounded-md shadow-md mt-2">
-              <div class="absolute left-0 inset-y-0 flex items-center  pl-1.5">
-                <svg class="text-gray-400 w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                  <path fill="currentColor" d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 14H4V8l8 5l8-5v10zm-8-7L4 6h16l-8 5z"/>
-                </svg>
-              </div>
-              <input
-                  v-model="field.value"
-                  :type="field.type"
-                  :id="field.name"
-                  :name="field.name"
-                  class="border-gray-300 px-7 rounded-md focus:border-gray-500 focus:ring-gray-500 text-sm text-gray-700 placeholder-gray-500 w-full"
-                  :class="{
-                      'border-red-500 focus:border-red-500': v$.formData[Object.keys(formData)[i]]?.value?.$error,
-                      'border-[#42d392] ': !v$.formData[Object.keys(formData)[i]]?.value?.$invalid,
-                    }"
-                  :placeholder="field.placeholder"
-                  @change="v$.formData[Object.keys(formData)[i]]?.value?.$touch"
-              >
-            </div>
-          </div>
-          <button
-              type="submit"
-              class="flex justify-center items-center  mx-auto rounded-md bg-green-600 py-2 px-4 text-white hover:bg-green-700 font-semibold
-          shadow-lg hover:shadow-xl focus:shadow-xl focus:outline-none focus:ring-2 focus:ring-green-500
-          focus:ring-offset-2 w-full transition duration-200 ease-in-out "
-          >
-            Submit
-          </button>
-        </form>
-        <div class="flex justify-center items-center mt-5">
-          <nuxt-link to="login" class="font-medium text-sm text-green-600 hover:text-green-700">Already have an account?</nuxt-link>
-        </div>
-      </div>
-    </main>
+    <h1 class="text-center text-4xl">This is main page</h1>
   </div>
 </template>
 
 <script >
-import {useVuelidate} from "@vuelidate/core"
-import { required, email, sameAs, minLength } from '@vuelidate/validators'
 
 export default {
-  setup () { return { v$: useVuelidate() } },
   data () {
     return {
-      signIn: true,
-      formData: {
-        email: {
-          name: 'Email',
-          type: 'email',
-          placeholder: 'email',
-          value: ''
-        },
-        password: {
-          name: 'Password',
-          type: 'password',
-          placeholder: 'min 4 characters',
-          value: ''
-        },
-        confirmPassword: {
-          name: 'Confirm password',
-          type: 'password',
-          placeholder: 'repeat password',
-          value: ''
-        }
-      }
-    }
-  },
-  validations () {
-    return {
-      formData :{
-        email: {
-          value: {
-            required, email
-          }
-        },
-        password: {
-          value: {
-            required, minLength: minLength(4)
-          }
-        },
-        confirmPassword: {
-          value: {
-            required,
-            sameAsPassword: sameAs(this.formData.password.value)
-          }
-        }
-      }
-    }
-  },
-  methods: {
-    submitRegistration () {
-      if (!this.v$.$invalid) {
-        const router = useRouter()
-        router.push('/login')
-      }
+      signIn: true
     }
   }
 }
--- pages/login.vue
+++ pages/login.vue
@@ -19,11 +19,11 @@
             autocomplete="off"
             class="space-y-6"
             novalidate
-            @submit.prevent="submitLogin"
+            @submit.prevent="submitLoginIn"
         >
 
           <!--          warning-->
-          <div class="flex gap-4 items-center justify-between border border-red-200 rounded p-3 bg-red-50">
+          <div v-show="!isLoginInFalse" class="flex gap-4 items-center justify-between border border-red-200 rounded p-3 bg-red-50">
             <svg class="text-red-500 mx-auto w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 11c-.55 0-1-.45-1-1V8c0-.55.45-1 1-1s1 .45 1 1v4c0 .55-.45 1-1 1zm1 4h-2v-2h2v2z"/>
             </svg>
@@ -31,9 +31,9 @@
             <p class="text-red-800">These credentials dont match our records.</p>
           </div>
 
-          <!--            input Email-->
-          <div>
-            <label class="block text-sm font-medium text-gray-700 pl-1" for="email">Email</label>
+          <!--            inputs-->
+          <div v-for="(field, i) in Object.values(loginData)" :key="i">
+            <label class="block text-sm font-medium text-gray-700 pl-1" :for="field.name">{{ field.name }}</label>
             <div class="relative rounded-md shadow-md mt-2">
               <div class="absolute left-0 inset-y-0 flex items-center  pl-1.5">
                 <svg class="text-gray-400 w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -41,68 +41,37 @@
                 </svg>
               </div>
               <input
-                  v-model="formData.email"
+                  v-model="field.value"
                   class="border-gray-300 pl-7 rounded-md focus:border-green-500 focus:ring-green-500 text-sm w-full"
                   :class="{
-                    'border-red-500 focus:border-red-500': v$.email.$error && !submitted,
-                    'border-[#42d392] ': !v$.email.$invalid
+                    'border-red-500 focus:border-red-500': v$?.loginData[Object.keys(loginData)[i]].value?.$error,
+                    'border-[#42d392] ': !v$?.loginData[Object.keys(loginData)[i]].value?.$invalid
                   }"
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="JohnDoe@gmail.com"
-                  @change="v$.email.$touch"
+                  :type="field.type"
+                  :id="field.name"
+                  :name="field.name"
+                  :placeholder="field.placeholder"
+                  @change="v$?.loginData[Object.keys(loginData)[i]].value?.$touch"
               />
-              <Icon
-                  v-if="v$?.email?.$dirty && v$.email.$error && !submitted"
-                  class="absolute right-2 h-full text-xl text-green-500"
-                  :class="{ 'text-green-500': !v$.email.$invalid, 'text-yellow-500': v$.email.$error }"
-                  :name="`heroicons-solid:${!v$.email.$error ? 'check-circle' : 'exclamation'}`"
-              />
+<!--             Helper Messages-->
               <div
-                  v-if="v$?.email?.$dirty && v$?.email?.$error"
-                  class="text-red-500 text-sm mt-3"
-              >{{ v$?.email?.$message }}</div>
-            </div>
-
-          </div>
-
-          <!--            input Password-->
-          <div>
-            <label class="block text-sm font-medium text-gray-700 pl-1" for="password">Password</label>
-            <div class="relative rounded-md shadow-md mt-2">
-              <div class="absolute left-0 inset-y-0 flex items-center  pl-1.5">
-                <svg class="text-gray-400 w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                  <path fill="currentColor" d="M6 22q-.825 0-1.413-.588T4 20V10q0-.825.588-1.413T6 8h1V6q0-2.075 1.463-3.538T12 1q2.075 0 3.538 1.463T17 6v2h1q.825 0 1.413.588T20 10v10q0 .825-.588 1.413T18 22H6Zm0-2h12V10H6v10Zm6-3q.825 0 1.413-.588T14 15q0-.825-.588-1.413T12 13q-.825 0-1.413.588T10 15q0 .825.588 1.413T12 17ZM9 8h6V6q0-1.25-.875-2.125T12 3q-1.25 0-2.125.875T9 6v2ZM6 20V10v10Z"/>
-                </svg>
-              </div>
-              <input
-                  v-model="formData.password"
-                  class="border-gray-300 pl-7 rounded-md focus:border-green-500 focus:ring-green-500 text-sm w-full"
-                  :class="{
-                    'border-red-500 focus:border-red-500': v$.password.$error,
-                    'border-[#42d392] ': !v$.password.$invalid,
-                  }"
-                  type="password"
-                  id="password"
-                  name="password"
-                  placeholder="min 4 characters"
-                  @change="v$.password.$touch"
-              >
-              <Icon
-                  v-if="(v$?.password?.$dirty || submitted) && v$.password.$error"
-                  class="absolute right-2 h-full text-xl text-green-500"
-                  :class="{ 'text-green-500': !v$.password.$invalid, 'text-yellow-500': v$.password.$error }"
-                  :name="`heroicons-solid:${!v$.password.$error ? 'check-circle' : 'exclamation'}`"
-              />
-              <p
-                  v-if="v$?.password?.$dirty && v$?.password?.$error"
+                  v-if="v$?.loginData[Object.keys(loginData)[i]].value?.$dirty && v$?.loginData[Object.keys(loginData)[i]].value?.$error"
                   class="text-red-500 text-sm mt-1"
-              >{{ v$?.password?.$message }}</p>
+              >{{ v$?.loginData[Object.keys(loginData)[i]].value?.$message }}</div>
+              <div>
+                <!--              <Icon-->
+                <!--                  v-if="v$.loginData[Object.keys(loginData)[i]].value?.$dirty && v$.loginData[Object.keys(loginData)[i]].value?.$error"-->
+                <!--                  class="absolute right-2 h-full text-xl text-green-500"-->
+                <!--                  :class="{ 'text-green-500': !v$.loginData[Object.keys(loginData)[i]].value?.$invalid,-->
+                <!--                  'text-yellow-500': v$.loginData[Object.keys(loginData)[i]].value?.$error }"-->
+                <!--                  :name="{'heroicons-solid' : !v$.loginData[Object.keys(loginData)[i]].value?.$error ? 'check-circle': v$.loginData[Object.keys(loginData)[i]].value?.$error }"-->
+                <!--              />-->
+              </div>
+
             </div>
 
           </div>
-
+<!--            Checkbox Remember me-->
           <div class="flex flex-col gap-3 justify-center items-center mt-5">
             <div class="flex gap-2 items-center justify-start">
 
@@ -118,21 +87,12 @@
             <nuxt-link to="/forgot-password" class="font-medium text-sm text-green-600 hover:text-green-700">Forgot your password?</nuxt-link>
           </div>
 
-          <div class="pt-5">
-<!--            <nuxt-link to="/forgot-password" class="flex justify-center items-center rounded-md bg-green-600 py-2 px-4 text-white hover:bg-green-700 font-semibold-->
-<!--          shadow-lg hover:shadow-xl focus:shadow-xl focus:outline-none focus:ring-2 focus:ring-green-500-->
-<!--          focus:ring-offset-2 transition duration-200 ease-in-out "-->
-<!--            >-->
-<!--              Sign in-->
-<!--            </nuxt-link>-->
-          </div>
           <button
               type="submit"
               class="flex justify-center items-center mx-auto rounded-md bg-green-600 py-2 px-4 text-white hover:bg-green-700 font-semibold
               shadow-lg hover:shadow-xl focus:shadow-xl focus:outline-none focus:ring-2 focus:ring-green-500
               focus:ring-offset-2 transition duration-200 ease-in-out"
           >
-            <!--              @click="$router.push('/forgot-password')"-->
             Submit
           </button>
 
@@ -144,56 +104,62 @@
 </template>
 
 
-<script setup>
-import { required, email, sameAs, minLength, helpers } from '@vuelidate/validators'
-import { useVuelidate } from '@vuelidate/core'
+<script>
+  import { required, email, helpers } from '@vuelidate/validators'
+  import { useVuelidate } from '@vuelidate/core'
 
-
-let submitted = false
-const formData = reactive({
-  email: '',
-  password: ''
-})
-const signUp = true
-
-const rules = computed(() => {
-  return {
-    email: {
-      required: helpers.withMessage('The email field is required', required),
-      email: helpers.withMessage('Invalid email format', email),
+  export default {
+    setup() {
+      return {v$: useVuelidate()}
     },
-    password: {
-      required: helpers.withMessage('The password field is required', required),
-      minLength: minLength(4),
+    data() {
+      return {
+        loginData: {
+          email: {
+            name: 'Email',
+            type: 'email',
+            placeholder: 'JoeDou@gmail.com',
+            value: ''
+          },
+          password: {
+            name: 'Password',
+            type: 'password',
+            placeholder: 'your password',
+            value: ''
+          },
+        },
+        signUp: true,
+        isLoginInFalse: true
+      }
+    },
+    validations() {
+      return {
+        loginData: {
+          email: {
+            value: {
+              required: helpers.withMessage('The email field is required', required),
+              email: helpers.withMessage('Invalid email format', email),
+            }
+          },
+          password: {
+            value: {
+              required: helpers.withMessage('The password field is required', required),
+            }
+          }
+        }
+      }
+    },
+    methods: {
+      submitLoginIn() {
+        if (!this.v$.$invalid) {
+          const router = useRouter()
+          router.push('/')
+        } else {
+          this.isLoginInFalse = false
+        }
+      }
     }
-    // confirmPassword: {
-    //   required: helpers.withMessage('The password confirmation field is required', required),
-    //   sameAs: helpers.withMessage("Passwords don't match", sameAs(formData.password)),
-    // },
   }
-})
-const v$ = useVuelidate(rules, formData)
-
-const submitLogin = () => {
-  if (v$.value.$invalid) return
-  submitted = true
-  formData.email = ''
-  formData.password = ''
-}
-
-
-
-// export default {
-//   name: "login",
-//   data () {
-//     return {
-//       signUp: true
-//     }
-//   }
-// }
-
-
-
 </script>
 
 <style scoped>
