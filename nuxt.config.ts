cc7f5b51377d7aedd57764792c9741e2912692a0
Yevgen <evghen.naydenko@gmail.com>
2023-08-20 17:00:53 +0300

    install gh-pages and added  predeploy": "nuxi generate",      "deploy": "gh-pages -d dist" to package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,5 +13,8 @@ export default defineNuxtConfig({
   tailwindcss: {
     configPath: '~/tailwind.config.js'
   }
-
+  // app: {
+  //   baseURL: '/myNuxt-for-test/', // baseURL: '/<repository>/'
+  //   buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  // }
 })
